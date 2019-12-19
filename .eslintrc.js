98f86072e9200bdefe52fd105a179800db663daa
Ram Dahal <ram.lall.dahal@ericsson.com>
2019-12-19 11:23:35 +0530

    Final Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,14 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
+  "overrides": [
+    {
+      "files": ["*.vue"],
+      "rules": {
+        "indent": "off"
+      }
+    }
+  ],
   parserOptions: {
     parser: 'babel-eslint'
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Adopt a new best friend. </h1>
+    <h1> Totel Pets Count: {{ animalsCount }} </h1>
+
+    <button @click="togglePetForm" class="btn btn-primary">Add new Pet</button>
+
+      <b-form @submit.prevent="handleSubmit" v-if="showpetForm">
+      <b-form-group id="input-group-1" label="Your Pet's Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="text"
+          v-model="formData.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Species:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="formData.species"
+          :options="['cats','dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Your Pet's Age:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="number"
+          v-model="formData.age"
+          required
+          placeholder="Enter Age"
+        ></b-form-input>
+      </b-form-group>
+
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      showpetForm: false,
+      formData:{
+        name:'',
+        age:0,
+        species:null
+      }
+
+    }
+
+  },
+  computed:{
+    ...mapGetters([
+      'animalsCount'
+    ])
+  },
+  methods:{
+    ...mapActions([
+        'addPet'
+    ]),
+    togglePetForm(){
+      this.showpetForm=!this.showpetForm
+    },
+    handleSubmit(){
+      const {species,name,age}=this.formData
+      const payload={
+        species,
+        pet:{
+          name,
+          age,
+
+        }
+      }
+      this.addPet(payload)
+      this.formData={
+        name:'',
+        age:0,
+        species:null
+
+      }
+    }
   }
 }
 </script>
